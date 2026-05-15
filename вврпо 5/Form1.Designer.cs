a30748f056e9e954f2d6cf1bcefd3d70989b2bc8
mariaromancik22@gmail.com <mariaromancik22@gmail.com>
2026-05-15 23:48:25 +0300

    Initial commit

---
+++ вврпо 5/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace вврпо_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameTable = new System.Windows.Forms.TableLayoutPanel();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button00 = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.gameTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameTable
+            // 
+            this.gameTable.ColumnCount = 3;
+            this.gameTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.Controls.Add(this.button22, 2, 2);
+            this.gameTable.Controls.Add(this.button21, 1, 2);
+            this.gameTable.Controls.Add(this.button20, 0, 2);
+            this.gameTable.Controls.Add(this.button12, 2, 1);
+            this.gameTable.Controls.Add(this.button11, 1, 1);
+            this.gameTable.Controls.Add(this.button10, 0, 1);
+            this.gameTable.Controls.Add(this.button02, 2, 0);
+            this.gameTable.Controls.Add(this.button01, 1, 0);
+            this.gameTable.Controls.Add(this.button00, 0, 0);
+            this.gameTable.Location = new System.Drawing.Point(39, 12);
+            this.gameTable.Name = "gameTable";
+            this.gameTable.RowCount = 3;
+            this.gameTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.gameTable.Size = new System.Drawing.Size(300, 296);
+            this.gameTable.TabIndex = 0;
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button22.Location = new System.Drawing.Point(201, 199);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(94, 92);
+            this.button22.TabIndex = 9;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button21.Location = new System.Drawing.Point(102, 199);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(93, 92);
+            this.button21.TabIndex = 8;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button20
+            // 
+            this.button20.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button20.Location = new System.Drawing.Point(3, 199);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(93, 92);
+            this.button20.TabIndex = 7;
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button12.Location = new System.Drawing.Point(201, 101);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(94, 92);
+            this.button12.TabIndex = 6;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button11.Location = new System.Drawing.Point(102, 101);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(93, 92);
+            this.button11.TabIndex = 5;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(3, 101);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(93, 92);
+            this.button10.TabIndex = 4;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button02
+            // 
+            this.button02.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button02.Location = new System.Drawing.Point(201, 3);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(94, 92);
+            this.button02.TabIndex = 3;
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button01
+            // 
+            this.button01.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button01.Location = new System.Drawing.Point(102, 3);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(93, 92);
+            this.button01.TabIndex = 2;
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.CellClick);
+            // 
+            // button00
+            // 
+            this.button00.Font = new System.Drawing.Font("Arial Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button00.Location = new System.Drawing.Point(3, 3);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(93, 92);
+            this.button00.TabIndex = 1;
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.CellClick);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.BackColor = System.Drawing.Color.GhostWhite;
+            this.statusLabel.Location = new System.Drawing.Point(42, 323);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(292, 20);
+            this.statusLabel.TabIndex = 1;
+            this.statusLabel.Text = "Нажмите \"Начать игру\"";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(141, 356);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(94, 46);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Начать игру";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(76, 408);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(94, 46);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Сохранить игру";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(204, 408);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(94, 46);
+            this.loadButton.TabIndex = 4;
+            this.loadButton.Text = "Загрузить игру";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(76, 474);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(94, 46);
+            this.newGameButton.TabIndex = 5;
+            this.newGameButton.Text = "Новая игра";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Visible = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(204, 474);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(94, 46);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Выйти";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(382, 655);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.gameTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.CellClick);
+            this.gameTable.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel gameTable;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button newGameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ вврпо 5/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace вврпо_5   // замените на своё пространство имён
+{
+    public partial class Form1 : Form
+    {
+        // Игровое поле (0-пусто, 1-игрок X, 2-компьютер O)
+        private int[] board = new int[9];
+        private Button[] cellButtons = new Button[9];
+        private Random random = new Random();
+
+        // Флаги состояния
+        private bool isPlayerTurn;    // true - игрок, false - компьютер
+        private bool gameActive;      // игра активна (идёт)
+        private bool gameOver;        // партия окончена
+        private bool nextFirstPlayer; // кто начнёт следующую игру: true-игрок, false-компьютер
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustom();
+            InitializeGameState();
+
+            // Настройка видимости кнопок
+            exitButton.Visible = true;
+            exitButton.Enabled = true;
+            newGameButton.Visible = false;  // сначала скрыта
+            startButton.Visible = true;
+            startButton.Enabled = true;
+        }
+
+        private void InitializeGameState()
+        {
+            nextFirstPlayer = true;   // первая игра – игрок ходит первым
+            ResetBoard();             // поле очищено и заблокировано
+            gameActive = false;
+            gameOver = false;
+            statusLabel.Text = "Нажмите 'Начать игру'";
+        }
+
+        private void ResetBoard()
+        {
+            for (int i = 0; i < 9; i++) board[i] = 0;
+            foreach (Button btn in cellButtons) btn.Text = "";
+            gameActive = false;
+            gameOver = false;
+            SetButtonsEnabled(false);
+        }
+
+        private void SetButtonsEnabled(bool enabled)
+        {
+            foreach (Button btn in cellButtons) btn.Enabled = enabled;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ResetBoard();
+            gameActive = true;
+            gameOver = false;
+            isPlayerTurn = nextFirstPlayer;
+            SetButtonsEnabled(true);
+            startButton.Visible = false;   // скрываем после первого старта
+            newGameButton.Visible = true;  // показываем кнопку "Новая игра"
+            statusLabel.Text = isPlayerTurn ? "Ваш ход (X)" : "Ход компьютера (O)...";
+            if (!isPlayerTurn) ComputerMove();
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            // Сразу начинаем новую игру
+            ResetBoard();
+            gameActive = true;
+            gameOver = false;
+            isPlayerTurn = nextFirstPlayer;
+            SetButtonsEnabled(true);
+            statusLabel.Text = isPlayerTurn ? "Ваш ход (X)" : "Ход компьютера (O)...";
+            if (!isPlayerTurn) ComputerMove();
+        }
+
+        private void CellClick(object sender, EventArgs e)
+        {
+            Button clicked = sender as Button;
+            if (!gameActive || gameOver || !isPlayerTurn) return;
+
+            int index = -1;
+            for (int i = 0; i < 9; i++)
+                if (cellButtons[i] == clicked) { index = i; break; }
+            if (index == -1) return;
+            if (board[index] != 0) return;
+
+            board[index] = 1;
+            clicked.Text = "X";
+            if (CheckGameOver()) return;
+
+            isPlayerTurn = false;
+            statusLabel.Text = "Ход компьютера (O)...";
+            ComputerMove();
+        }
+
+        private async void ComputerMove()
+        {
+            if (!gameActive || gameOver || isPlayerTurn) return;
+
+            statusLabel.Text = "Ход компьютера (O)...";
+            await Task.Delay(500);
+
+            if (!gameActive || gameOver || isPlayerTurn) return;
+
+            // Сбор свободных клеток
+            var freeIndices = new System.Collections.Generic.List<int>();
+            for (int i = 0; i < 9; i++) if (board[i] == 0) freeIndices.Add(i);
+
+            if (freeIndices.Count == 0)
+            {
+                CheckGameOver();
+                return;
+            }
+
+            int move = freeIndices[random.Next(freeIndices.Count)];
+            board[move] = 2;
+            cellButtons[move].Text = "O";
+
+            if (CheckGameOver()) return;
+
+            isPlayerTurn = true;
+            statusLabel.Text = "Ваш ход (X)";
+        }
+
+        private bool CheckGameOver()
+        {
+            string winner = GetWinner();
+            if (winner != null)
+            {
+                EndGame(winner);
+                return true;
+            }
+            bool hasEmpty = false;
+            for (int i = 0; i < 9; i++) if (board[i] == 0) { hasEmpty = true; break; }
+            if (!hasEmpty)
+            {
+                EndGame("Draw");
+                return true;
+            }
+            return false;
+        }
+
+        private string GetWinner()
+        {
+            int[][] winLines = {
+                new int[] {0,1,2}, new int[] {3,4,5}, new int[] {6,7,8},
+                new int[] {0,3,6}, new int[] {1,4,7}, new int[] {2,5,8},
+                new int[] {0,4,8}, new int[] {2,4,6}
+            };
+            foreach (var line in winLines)
+            {
+                int a = board[line[0]], b = board[line[1]], c = board[line[2]];
+                if (a != 0 && a == b && b == c)
+                    return a == 1 ? "Player" : "Computer";
+            }
+            return null;
+        }
+
+        private void EndGame(string winner)
+        {
+            gameActive = false;
+            gameOver = true;
+            SetButtonsEnabled(false);
+            if (winner == "Player")
+            {
+                statusLabel.Text = "Игрок победил!";
+                nextFirstPlayer = false; // компьютер начинает следующую
+            }
+            else if (winner == "Computer")
+            {
+                statusLabel.Text = "Компьютер победил!";
+                nextFirstPlayer = true;  // игрок начинает следующую
+            }
+            else
+            {
+                statusLabel.Text = "Ничья!";
+                // nextFirstPlayer не меняется
+            }
+            // Кнопка "Начать игру" уже скрыта, "Новая игра" видна, "Выход" виден всегда
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e) => SaveGame();
+        private void LoadButton_Click(object sender, EventArgs e) => LoadGame();
+
+        [Serializable]
+        private class GameSaveData
+        {
+            public int[] Board { get; set; }
+            public bool IsPlayerTurn { get; set; }
+            public bool IsGameOver { get; set; }
+            public string GameOverMessage { get; set; }
+            public bool NextFirstPlayer { get; set; }
+        }
+
+        private void SaveGame()
+        {
+            try
+            {
+                var data = new GameSaveData
+                {
+                    Board = board,
+                    IsPlayerTurn = isPlayerTurn,
+                    IsGameOver = gameOver,
+                    GameOverMessage = gameOver ? statusLabel.Text : null,
+                    NextFirstPlayer = nextFirstPlayer
+                };
+                string json = JsonSerializer.Serialize(data);
+                File.WriteAllText("savegame.json", json);
+                MessageBox.Show("Сохранено в savegame.json", "Сохранение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}");
+            }
+        }
+
+        private void LoadGame()
+        {
+            if (!File.Exists("savegame.json"))
+            {
+                MessageBox.Show("Файл не найден.");
+                return;
+            }
+            try
+            {
+                string json = File.ReadAllText("savegame.json");
+                var data = JsonSerializer.Deserialize<GameSaveData>(json);
+                if (data == null) throw new Exception("Неверный формат");
+
+                board = data.Board;
+                isPlayerTurn = data.IsPlayerTurn;
+                gameOver = data.IsGameOver;
+                nextFirstPlayer = data.NextFirstPlayer;
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i] == 1) cellButtons[i].Text = "X";
+                    else if (board[i] == 2) cellButtons[i].Text = "O";
+                    else cellButtons[i].Text = "";
+                }
+
+                if (gameOver)
+                {
+                    gameActive = false;
+                    SetButtonsEnabled(false);
+                    statusLabel.Text = data.GameOverMessage ?? "Игра окончена";
+                    startButton.Visible = false;
+                    newGameButton.Visible = true;
+                    // exitButton.Visible оставляем как есть (true)
+                }
+                else
+                {
+                    gameActive = true;
+                    SetButtonsEnabled(true);
+                    statusLabel.Text = isPlayerTurn ? "Ваш ход (X)" : "Ход компьютера (O)...";
+                    startButton.Visible = false;
+                    newGameButton.Visible = true;
+                    if (!isPlayerTurn) ComputerMove();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки: {ex.Message}");
+            }
+        }
+
+        // Этот метод должен быть реализован, если вы используете TableLayoutPanel для поиска кнопок
+        private void InitializeCustom()
+        {
+            // Если вы используете способ поиска кнопок через TableLayoutPanel:
+            int index = 0;
+            foreach (Control ctrl in gameTable.Controls) // замените на реальное имя таблицы
+            {
+                if (ctrl is Button btn)
+                {
+                    cellButtons[index++] = btn;
+                    btn.Click += CellClick;
+                }
+            }
+            // Если вы переименовали кнопки как button00...button22, то можно и так:
+            // cellButtons[0] = button00;
+            // ... и так далее, но тогда нужно заполнить все 9.
+
+            // Подписка на события кнопок управления
+            startButton.Click += StartButton_Click;
+            saveButton.Click += SaveButton_Click;
+            loadButton.Click += LoadButton_Click;
+            newGameButton.Click += NewGameButton_Click;
+            exitButton.Click += (s, e) => Application.Exit();
+        }
+    }
+}
